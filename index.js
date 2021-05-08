6ba665427e230d1a85442a2cb4eb56626e035ed4
Gaurav <gauravrai575@gmail.com>
2021-05-08 21:31:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+getUpdate = () => {
+    console.log("Updating list....");
+    tit = document.getElementById("title").value;
+    desc = document.getElementById("description").value;
+    if (localStorage.getItem("itemsJson") == null){
+        itemJsonArray = [];
+        itemJsonArray.push([tit, desc]);
+        localStorage.setItem("itemsJson" , JSON.stringify(itemJsonArray))
+
+    }
+    else{
+        itemJsonArrayStr = localStorage.getItem("itemsJson");
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+        itemJsonArray.push([tit, desc]);
+        localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+    }
+    Update()
+
+}
+
+
+Update = () => {
+    if (localStorage.getItem("itemsJson") == null){
+        itemJsonArray = [];
+        localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray))
+    }
+    else{
+        itemJsonArrayStr = localStorage.getItem("itemsJson");
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+    }
+
+    // po[ulate the table
+    let tableBody = document.getElementById("tableBody");
+    let str = "";
+    itemJsonArray.forEach((element, index) => {
+        str += ` <tr>
+        <th scope="row">${index + 1}</th>
+        <td>${element[0]}</td>
+        <td>${element[1]}</td>
+        <td><button class="btn btn-primary" onclick ="deleted(${index})"> Delete</button></td>
+    </tr>`
+    });
+    tableBody.innerHTML = str;
+}
+add = document.getElementById("add");
+add.addEventListener("click", getUpdate);
+Update()
+
+function deleted(itemIndex) {
+    console.log('Delete', itemIndex);
+    itemJsonArrayStr = localStorage.getItem('itemsJson')
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+
+    // Delet itemIndex element from the array
+    itemJsonArray.splice(itemIndex, 1)
+    localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+
+    Update();
+}
+
+
+    
+
